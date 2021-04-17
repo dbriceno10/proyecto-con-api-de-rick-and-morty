a9780fa647e456d5f1dc32ba4ea1377b9064e1bb
nombre <daniel.b100726@gmail.com>
2021-04-16 21:55:09 -0400

    Haciendo cambios para trabajar por id

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
-// const fetchData = require('../src/utils/fetchData')
-const API = "https://rickandmortyapi.com/api/character/"
+const returnApiCharacter = (id) => `https://rickandmortyapi.com/api/character/${id}`
+async function returnEpisode (url) {
+    const response = await fetch(url)
+    const data = await response.json()
+    try {
+        console.log(data.id)
+    } catch(error) {
+        console.error(error)
+    }
+}
+
 let nombre = document.getElementById("name")
 let foto = document.getElementById("photo")
 let especie = document.getElementById("specie")
@@ -9,42 +18,82 @@ let origen = document.getElementById("origin")
 let ubicacion = document.getElementById("location")
 let firstAp = document.getElementById("first-ap")
 let lastAp = document.getElementById("last-ap")
-let i = 3
 
-async function fetchData(API) {
+async function fetchData(id) {
+    const API = returnApiCharacter(id)
 	try {
 		const response = await fetch(API)
 		const data = await response.json()
-        const response2 = await fetch(`${data.results[i].episode[0]}`)
-        data2 = await response2.json()
-        console.log(`${data.results[i].episode.length}`)
-        console.log(data2.id)
-        const last = data.results[i].episode.length - 1
-        const response3 = await fetch(`${data.results[i].episode[last]}`)
-        const data3 = await response3.json()
-		// console.log(data);
-        // console.log(data.results[i])
-        nombre.innerHTML = `Name: ${data.results[i].name}`
-        foto.innerHTML = `<img src="${data.results[i].image}" alt="${data.results[i].name}">`
-        especie.innerHTML = `Specie: ${data.results[i].species}`
-        genero.innerHTML = `Gender: ${data.results[i].gender}`
-        estatus.innerHTML = `Status: ${data.results[i].status}`
-        
-        if(data.results[i].location.name != data.results[i].origin.name) {
-            origen.innerHTML = `Origin: ${data.results[i].origin.name}`
-            ubicacion.innerHTML = `Actual Locate: ${data.results[i].location.name}`
+        nombre.innerHTML = `Name: ${data.name}`
+        foto.innerHTML = `<img src="${data.image}" alt="${data.name}">`
+        especie.innerHTML = `Specie: ${data.species}`
+        genero.innerHTML = `Gender: ${data.gender}`
+        estatus.innerHTML = `Status: ${data.status}`
+        if(data.location.name != data.origin.name) {
+            origen.innerHTML = `Origin: ${data.origin.name}`
+            ubicacion.innerHTML = `Actual Locate: ${data.location.name}`
         } else {
-            origen.innerHTML = `Origin: ${data.results[i].origin.name}`
+            origen.innerHTML = `Origin: ${data.origin.name}`
         }
-        firstAp.innerHTML = `First Aparition: ${data2.name} (${data2.episode}) On air: ${data2.air_date}`
-        if(data3.id != data2.id) {
-            lastAp.innerHTML = `Last Aparition: ${data3.name} (${data3.episode}) On air: ${data3.air_date}`
+        const firstAparition = data.episode[0]
+        const LastAparition = data.episode[data.episode.length - 1]
+        console.log(data.episode.length)
+        console.log((data.episode[data.episode.length - 1]))
+        
+        const responseF = await fetch(firstAparition)
+        const dataF = await responseF.json()
+        try {
+            console.log(dataF.id)
+        } catch(error) {
+            console.error(error)
         }
+
+        const responseL = await fetch(LastAparition)
+        const dataL = await responseL.json()
+        try {
+            console.log(dataL.id)
+        } catch(error) {
+            console.error(error)
+        }
+        // firstAp.innerHTML = `First Aparition: ${data2.name} (${data2.episode}) On air: ${data2.air_date}`
+        // if(data3.id != data2.id) {
+        //     lastAp.innerHTML = `Last Aparition: ${data3.name} (${data3.episode}) On air: ${data3.air_date}`
+        // }
+        
+        // firstAp.innerHTML = `First Aparition: ${data2.name} (${data2.episode}) On air: ${data2.air_date}`
+        // if(data3.id != data2.id) {
+        //     lastAp.innerHTML = `Last Aparition: ${data3.name} (${data3.episode}) On air: ${data3.air_date}`
+        // }
+        // const response2 = await fetch(`${data.results[i].episode[0]}`)
+        // data2 = await response2.json()
+        // console.log(`${data.results[i].episode.length}`)
+        // console.log(data2.id)
+        // const last = data.results[i].episode.length - 1
+        // const response3 = await fetch(`${data.results[i].episode[last]}`)
+        // const data3 = await response3.json()
+		// // console.log(data);
+        // // console.log(data.results[i])
+        // nombre.innerHTML = `Name: ${data.results[i].name}`
+        // foto.innerHTML = `<img src="${data.results[i].image}" alt="${data.results[i].name}">`
+        // especie.innerHTML = `Specie: ${data.results[i].species}`
+        // genero.innerHTML = `Gender: ${data.results[i].gender}`
+        // estatus.innerHTML = `Status: ${data.results[i].status}`
+        
+        // if(data.results[i].location.name != data.results[i].origin.name) {
+        //     origen.innerHTML = `Origin: ${data.results[i].origin.name}`
+        //     ubicacion.innerHTML = `Actual Locate: ${data.results[i].location.name}`
+        // } else {
+        //     origen.innerHTML = `Origin: ${data.results[i].origin.name}`
+        // }
+        // firstAp.innerHTML = `First Aparition: ${data2.name} (${data2.episode}) On air: ${data2.air_date}`
+        // if(data3.id != data2.id) {
+        //     lastAp.innerHTML = `Last Aparition: ${data3.name} (${data3.episode}) On air: ${data3.air_date}`
+        // }
     } catch (error) {
 		console.log(error)
 	}
 }
-fetchData(API)
+fetchData(1)
 
 // const data = fetch(API)
 // data

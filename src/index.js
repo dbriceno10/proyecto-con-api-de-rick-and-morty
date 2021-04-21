ca09c5b39e190b779bc33e72302e887220d1a7d9
nombre <daniel.b100726@gmail.com>
2021-04-20 22:02:53 -0400

    mejorando accesabilidad

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 // import { returnApiCharacter, fetchData, countId, callToAction, changeButton, keyboard} from "../src/app.js"
 window.onload = () => {
-    templateName = document.getElementById("name")
-    templateImg= document.getElementById("photo")
-    templateSpecies = document.getElementById("specie")
-    templateGender = document.getElementById("gender")
-    templateStatus = document.getElementById("status")
-    templateOrigin= document.getElementById("origin")
-    templateLocation = document.getElementById("location")
-    firstAp = document.getElementById("first-ap")
-    lastAp = document.getElementById("last-ap")
+    // templateName = document.getElementById("name")
+    // templateImg= document.getElementById("photo")
+    // templateSpecies = document.getElementById("specie")
+    // templateGender = document.getElementById("gender")
+    // templateStatus = document.getElementById("status")
+    // templateOrigin= document.getElementById("origin")
+    // templateLocation = document.getElementById("location")
+    // firstAp = document.getElementById("first-ap")
+    // lastAp = document.getElementById("last-ap")
     // placeholderBackground = document.getElementById("placeholder-background")
     document.onkeydown = keyboard
 }
+let templateName = document.getElementById("name")
+let templateImg= document.getElementById("photo")
+let templateSpecies = document.getElementById("specie")
+let templateGender = document.getElementById("gender")
+let templateStatus = document.getElementById("status")
+let templateOrigin= document.getElementById("origin")
+let templateLocation = document.getElementById("location")
+let firstAp = document.getElementById("first-ap")
+let lastAp = document.getElementById("last-ap") 
 const API = `https://rickandmortyapi.com/api/character/`
 let maxId = 0
 let operador = "clear"
